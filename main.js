c9bf3037288a74c6e0a28615b2378b04915e12bc
JeffVG7 <114269299+JeffVG7@users.noreply.github.com>
2023-01-24 13:44:51 -0500

    Arreglos
    agregue un boton para recargar la imagen y lo centre, agregue un css para el diseño del boton y futuros diseños y le agregue bostrap para organizar tanto en elseño y el orden como el responsive en un futuro

--- main.js
+++ main.js
@@ -4,9 +4,20 @@ console.log("hello world!")
 const URL = "https://api.thecatapi.com/v1/images/search";
 
 //aqui estamos sacando todos los valores que sacaremos de la api, al solo sacar imagenes aleatorias solo estamos estamos extrayendo la direccion de cada imagen
-fetch(URL)
-.then(res => res.json())
-.then(data => {
-    const img = document.querySelector("img");
+async function myCat () {
+    //aqui estoy extrayendo la api
+    const res = await fetch(URL);
+
+    //aqui estoy traduciendo lo extraido
+    const data = await res.json();
+
+    //aqui estoy extrayendo parte de la extraccion
+    const img = document.querySelector('img');
+
+    //aqui estoy extrayendo lo que nesesito de lo que extrai, osea el atributo cero que es la direccion de la imagen
     img.src = data[0].url;
-} )
+}
+
+// esto es para habilitar un boton que recargue las imagenes
+const myButton = document.querySelector("button");
+myButton.onclick = myCat;
